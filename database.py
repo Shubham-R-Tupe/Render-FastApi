167798ff78dd1068b76155e8f4c1eb6ddfc799b7
Shubham-R-Tupe <tupeshubhi@gmail.com>
2026-08-31 16:41:39 +0530

    Ready To Deploy

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:Iamshubham77@localhost:5432/blog_db"
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "postgresql://postgres:Iamshubham77@localhost:5432/blog_db"
+)
 
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
-
